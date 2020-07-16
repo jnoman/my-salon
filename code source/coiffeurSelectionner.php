6e01df4c25be4fec3c6fadb1dd1daaac9e9c75e7
jnoman <jamalnoman62@gmail.com>
2020-07-16 13:56:30 +0100

    terminer page list coiffeurs

---
+++ code source/coiffeurSelectionner.php
@@ -0,0 +1,34 @@
+<?php
+    require 'include/header.php';
+    if(!isset($_SESSION['id']))
+    {
+        echo "<script>{window.location.href = 'connection.php'};</script>";
+    }
+    elseif($_SESSION['type'] != "client")
+    {
+        echo "<div class='messageErreur'><p class='text-center font-weight-bolder'>Vous n'êtes pas autorisé à accéder à cette page</p></div>";
+    }
+    elseif(isset($_GET["idSalon"]) && !empty( $_GET['idSalon'] ))
+    {
+        require 'include/database.php';
+        $id=$_SESSION['id'];
+        $idSalon=$_GET["idSalon"];
+        $db = Database::connect();
+        $statement = $db->prepare("SELECT * FROM salon WHERE id_salon=$idSalon and date_fin>=CURRENT_DATE");
+        $statement->execute();
+        if($item = $statement->fetch())
+        {
+            echo 'yes';
+        }
+        else {
+            echo '<div class="messageErreur"><p class="text-center font-weight-bolder">id de salon n\'est pas valid</p></div>';
+        }
+        Database::disconnect();
+        
+    }
+    else 
+    {
+        echo "<div class='messageErreur'><p class='text-center font-weight-bolder'>Aucune id de salon sélectionner</p></div>";
+    }
+    require 'include/footer.php';
+?>
--- code source/listCoiffeurs.php
+++ code source/listCoiffeurs.php
@@ -25,36 +25,28 @@
                 </form>
             </ul>
             <form class="form-inline my-2 my-lg-0" method="POST" action="">
-                <input class="form-control mr-sm-2" type="search" placeholder="nom salon" name="nomSalon" pattern="^[a-zA-Z_\- \']{3,}$" minlength="3" title="Le nom de la salon contient au moins 3 caractères" required ';if(isset($_GET["nomSalon"]) && !empty( $_GET['nomSalon'] )){echo 'value="';echo $_GET['nomSalon'].'"';} echo '>
+                <input class="form-control mr-sm-2" type="search" placeholder="nom salon" name="nomSalon" pattern="^[a-zA-Z_\- \']{0,}$" title="Le nom de la salon contient juste les caractères" ';if(isset($_GET["nomSalon"]) && !empty( $_GET['nomSalon'] )){echo 'value="';echo $_GET['nomSalon'].'"';} echo '>
                 <button class="btn btn-outline-success my-2 my-sm-0" name="chercher" type="submit">Chercher</button>
             </form>
         </div>
     </nav>';
+    $sql="SELECT id_salon,image,nom_salon FROM salon WHERE date_fin>=CURRENT_DATE";
     if(isset($_GET["nomSalon"]) && !empty( $_GET['nomSalon'] ))
     {
         $nomCoiffeur=$_GET["nomSalon"];
-        $statement = $db->prepare("SELECT id_salon,image,nom_salon FROM salon WHERE date_fin>=CURRENT_DATE and nom_salon LIKE '%$nomCoiffeur%'");
+        $sql = $sql." and nom_salon LIKE '%$nomCoiffeur%'";
     }
-    elseif(isset($_GET["genre"]) && !empty( $_GET['genre'] ) && isset($_GET["ville"]) && !empty( $_GET['ville'] ))
+    if(isset($_GET["genre"]) && !empty( $_GET['genre'] ))
     {
         $genre=$_GET["genre"];
-        $ville=$_GET["ville"];
-        $statement = $db->prepare("SELECT id_salon,image,nom_salon FROM salon WHERE date_fin>=CURRENT_DATE and genre='$genre' and ville='$ville'");
+        $sql = $sql." and genre='$genre'";
     }
-    elseif(isset($_GET["genre"]) && !empty( $_GET['genre'] ))
-    {
-        $genre=$_GET["genre"];
-        $statement = $db->prepare("SELECT id_salon,image,nom_salon FROM salon WHERE date_fin>=CURRENT_DATE and genre='$genre'");
-    }
-    elseif(isset($_GET["ville"]) && !empty( $_GET['ville'] ))
+    if(isset($_GET["ville"]) && !empty( $_GET['ville'] ))
     {
         $ville=$_GET["ville"];
-        $statement = $db->prepare("SELECT id_salon,image,nom_salon FROM salon WHERE date_fin>=CURRENT_DATE and ville='$ville'");
+        $sql = $sql. " and ville='$ville'";
     }
-    else
-    {
-        $statement = $db->prepare("SELECT id_salon,image,nom_salon FROM salon WHERE date_fin>=CURRENT_DATE");
-    }
+    $statement = $db->prepare($sql);
     $statement->execute();
     if($statement->rowCount() > 0)
     {
@@ -62,14 +54,16 @@
             <div class="row row-cols-1 row-cols-md-3">';
             while($row = $statement->fetch()) {
                 echo '<form method="POST" action="" name="search-form">
-                    <div class="col mb-4" onClick="submitdiv('.$row['id_salon'].')">
-                        <div class="card standardCard">
-                            <img src="data:image/jpeg;base64,'.base64_encode($row['image'] ).'" class="card-img-top" style="width: 100%;height:200px;margin-bottom: 50px;">
-                            <div class="card-body">
-                            <h5 class="card-title">'.$row['nom_salon'].'</h5>
+                    <a href="coiffeurSelectionner.php?idSalon='.$row['id_salon'].'" class="divsubmit">
+                        <div class="col mb-4">
+                            <div class="card standardCard">
+                                <img src="data:image/jpeg;base64,'.base64_encode($row['image'] ).'" class="card-img-top" style="width: 100%;height:200px;margin-bottom: 50px;">
+                                <div class="card-body">
+                                <h5 class="card-title">'.$row['nom_salon'].'</h5>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </form>';
             }
         echo '</div></div>';
@@ -83,67 +77,63 @@
     if(isset($_POST['chercher']))
     {
         $nomSalon=$_POST['nomSalon'];
-        echo "<script>window.location.href = 'listCoiffeurs.php?nomSalon=$nomSalon';</script>";
+        echo "<script>window.location.href = 'listCoiffeurs.php?";
+        if($nomSalon!="")
+        {
+            echo "nomSalon=$nomSalon";
+        }
+        if(isset($_GET["genre"]) && !empty( $_GET['genre'] ))
+        {
+            $genre=$_GET["genre"];
+            echo "&genre=$genre";
+        }
+        if(isset($_GET["ville"]) && !empty( $_GET['ville'] ))
+        {
+            $ville=$_GET["ville"];
+            echo "&ville=$ville";
+        }
+        echo "';</script>";
     }
     if(isset($_POST['genre']))
     {
         $genre=$_POST['genre'];
+        echo "<script>window.location.href = 'listCoiffeurs.php?";
+        if(isset($_GET["nomSalon"]) && !empty( $_GET['nomSalon'] ))
+        {
+            $nomSalon=$_GET["nomSalon"];
+            echo "nomSalon=$nomSalon";
+        }
+        if($genre!="sélectionner genre")
+        {
+            echo "&genre=$genre";
+        }
         if(isset($_GET["ville"]) && !empty( $_GET['ville'] ))
         {
             $ville=$_GET["ville"];
-            if($genre!="sélectionner genre")
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php?genre=$genre&ville=$ville';</script>";
-            }
-            else
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php?ville=$ville';</script>";
-            }
+            echo "&ville=$ville";
         }
-        else
-        {
-            if($genre!="sélectionner genre")
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php?genre=$genre';</script>";
-            }
-            else
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php';</script>";
-            }
-        }
+        echo "';</script>";
     }
     if(isset($_POST['ville']))
     {
         $ville=$_POST['ville'];
+        echo "<script>window.location.href = 'listCoiffeurs.php?";
+        if(isset($_GET["nomSalon"]) && !empty( $_GET['nomSalon'] ))
+        {
+            $nomSalon=$_GET["nomSalon"];
+            echo "nomSalon=$nomSalon";
+        }
         if(isset($_GET["genre"]) && !empty( $_GET['genre'] ))
         {
             $genre=$_GET["genre"];
-            if($genre!="sélectionner genre")
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php?genre=$genre&ville=$ville';</script>";
-            }
-            else
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php?genre=$genre';</script>";
-            }
+            echo "&genre=$genre";
         }
-        else
+        if($ville!="sélectionner votre ville")
         {
-            if($ville!="sélectionner genre")
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php?ville=$ville';</script>";
-            }
-            else
-            {
-                echo "<script>window.location.href = 'listCoiffeurs.php';</script>";
-            }
+            echo "&ville=$ville";
         }
+        echo "';</script>";
     }
 
     require 'include/footer.php';
 ?>
-<script>
-    function submitdiv(params) {
-        window.location.href = 'coiffeurSelectionner.php?idSalon='+params;
-    }
-</script>
